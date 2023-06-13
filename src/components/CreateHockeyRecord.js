464075c658888e7866dc8600de073a07f3a7e58d
Vinnie Pauley <vinniepauley@gmail.com>
2023-06-13 17:35:16 -0500

    path improvements and new component

---
+++ src/components/CreateHockeyRecord.js
@@ -0,0 +1,5 @@
+const CreateHockeyRecord = () => {
+  return <div>Create Hockey Record</div>
+}
+
+export default CreateHockeyRecord
--- src/routes/routes/CreateRecords.js
+++ src/routes/routes/CreateRecords.js
@@ -1,5 +1,12 @@
+import CreateHockeyRecords from 'components/CreateHockeyRecord'
+
 const CreateRecords = () => {
-  return <div>Create Records route</div>
+  return (
+    <div>
+      Create Records route
+      <CreateHockeyRecords />
+    </div>
+  )
 }
 
 export default CreateRecords
