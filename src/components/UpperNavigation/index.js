34323bc2263019cb951a96a806a95d4a0d749156
Vinnie Pauley <vinniepauley@gmail.com>
2023-06-13 16:39:39 -0500

    button navigation styled for active route

--- src/components/UpperNavigation/index.js
+++ src/components/UpperNavigation/index.js
@@ -1,16 +1,54 @@
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink, NavLink, useLocation } from 'react-router-dom'
+import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import { amber } from '@mui/material/colors'
+
+const ColoredRouteButton = props => {
+  const { title, route } = props
+
+  return (
+    <Button
+      sx={{
+        backgroundColor: amber[500],
+        '&:hover': {
+          backgroundColor: amber[700]
+        },
+        color: 'black'
+      }}
+      varient="contained"
+      component={RouterLink}
+      to={route}
+    >
+      {title}
+    </Button>
+  )
+}
 
 const UpperNavigation = () => {
+  const location = useLocation()
+
+  const { pathname } = location
   return (
-    <div>
-      <Button component={RouterLink} to="/">
-        Home
-      </Button>
-      <Button component={RouterLink} to="/records">
-        Records
-      </Button>
-    </div>
+    <Box
+      sx={{
+        padding: '1rem 0'
+      }}
+    >
+      {pathname === '/' ? (
+        <ColoredRouteButton title="Home" route="/" />
+      ) : (
+        <Button component={RouterLink} to="/">
+          Home
+        </Button>
+      )}
+      {pathname === '/records' ? (
+        <ColoredRouteButton title="Records" route="/records" />
+      ) : (
+        <Button component={RouterLink} to="/records">
+          Records
+        </Button>
+      )}
+    </Box>
   )
 }
 
