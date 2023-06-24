f88bfeeac4cd5b226db05ada55f47d303d580eaa
Vinnie Pauley <vinniepauley@gmail.com>
2023-06-24 13:40:03 -0500

    cleanup main form

--- src/components/CreateHockeyRecord.js
+++ src/components/CreateHockeyRecord.js
@@ -1,36 +1,48 @@
 import { useState } from 'react'
-import TextField from '@mui/material/TextField'
 
 import ValidatedTextInput from './shared/ValidatedTextInput'
 
 const CreateHockeyRecord = () => {
   const [homeTeam, setHomeTeam] = useState('')
-
-  const handleChange = e => {
-    setHomeTeam(e.target.value)
-  }
+  const [awayTeam, setAwayTeam] = useState('')
 
   const TeamNameValidation = teamName => {
     return /^[a-z]{3,40}$/i.test(teamName)
   }
 
+  const homeTeamHandler = result => {
+    const homeTeam = result
+
+    setHomeTeam(homeTeam.value)
+
+    console.log({ homeTeam })
+  }
+
   const awayTeamHandler = result => {
     const awayTeam = result
+
+    setAwayTeam(awayTeam.value)
 
     console.log({ awayTeam })
   }
 
   return (
     <div>
+      <p>{awayTeam ? awayTeam : 'No Entry'}</p>
       <ValidatedTextInput
         label="Away Team"
         validator={TeamNameValidation}
         handler={awayTeamHandler}
       />
 
-      <p>{homeTeam}</p>
+      <p>{homeTeam ? homeTeam : 'No Entry'}</p>
+      <ValidatedTextInput
+        label="Home Team"
+        validator={TeamNameValidation}
+        handler={homeTeamHandler}
+      />
 
-      <TextField label="Home Team" varient="standard" onChange={handleChange} />
+      {/* TODO: ranged number inputs */}
     </div>
   )
 }
